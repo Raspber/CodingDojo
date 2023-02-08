c0bf76df399b6ae3889bbe0f1a0b438b702ec984
Hauvert Pacheco <hpachec1@gmail.com>
2023-02-08 12:31:44 -0800

    commit

--- Python_Fundamentals/python_fundamentals/basketball_dictionaries.py
+++ Python_Fundamentals/python_fundamentals/basketball_dictionaries.py
@@ -36,3 +36,43 @@ players = [
         "team": "Chicago Bulls"
     }
 ]
+
+
+class Player:
+    def __init__(self, player_dict):
+        self.name = player_dict['name']
+        self.age = player_dict['age']
+        self.position = player_dict['position']
+        self.team = player_dict['team']
+
+
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets"
+}
+
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets"
+}
+player_kevin = Player(kevin)
+player_jason = Player(jason)
+player_kyrie = Player(kyrie)
+
+# ... (class definition and large list of players here)
+new_team = []
+# Write your for loop here to populate the new_team variable with Player objects.
+# for val in list
+for player_dict in players:
+    player = Player(player_dict)
+    new_team.append(player)
