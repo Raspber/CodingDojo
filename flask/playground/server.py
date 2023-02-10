dd0795eeb377ea93e5eaf2f740895dae0b754f30
Hauvert Pacheco <hpachec1@gmail.com>
2023-02-09 16:10:19 -0800

    commit

--- flask/playground/server.py
+++ flask/playground/server.py
@@ -7,9 +7,12 @@ def play():
     return render_template("play.html", phrase="hello", times=5)	# notice the 2 new named arguments!
 
 @app.route('/play/<int:num>')
-def play(num):
+def play2(num):
     return render_template('play2.html', num = num)
 
+@app.route('/play/<int:num>/<string:color>')
+def play3(num, color):
+    return render_template("play3.html",num = num, color = color)
 
 
 if __name__=="__main__":
