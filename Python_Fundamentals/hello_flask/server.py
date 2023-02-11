bfe87f21ded6be7d229e81ed6b6021a73fcf933f
Hauvert Pacheco <hpachec1@gmail.com>
2023-02-11 11:46:06 -0800

    commit

--- Python_Fundamentals/hello_flask/server.py
+++ Python_Fundamentals/hello_flask/server.py
@@ -1,30 +1,48 @@
 from flask import Flask, render_template  # added render_template!
-app = Flask(__name__)  
+app = Flask(__name__)
 
 
 # The "@" decorator associates this route with the function immediately following
-@app.route('/')                           
+@app.route('/')
 def index():
-    # Instead of returning a string, 
+    # Instead of returning a string,
     # we'll return the result of the render_template method, passing in the name of our HTML file
-    return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
+    # notice the 2 new named arguments!
+    return render_template("index.html", phrase="hello", times=5)
 
 # import statements, maybe some other routes
+
+
 @app.route('/success')
 def success():
     return "success"
 
 
-@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+# for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+@app.route('/hello/<name>')
 def hello(name):
     print(name)
     return "Hello, " + name
 
-@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+
+# for a route '/users/____/____', two parameters in the url get passed as username and id
+@app.route('/users/<username>/<id>')
 def show_user_profile(username, id):
     print(username)
     print(id)
     return "username: " + username + ", id: " + id
+
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template("lists.html", random_numbers=[3, 1, 5], students=student_info)
 
 
 # app.run(debug=True) should be the very last statement!
