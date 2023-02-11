bccad52a954a2b8e70aaaae730c6cdb05ddf64df
Hauvert Pacheco <hpachec1@gmail.com>
2023-02-11 11:31:49 -0800

    commit

--- Python_Fundamentals/hello_flask/server.py
+++ Python_Fundamentals/hello_flask/server.py
@@ -1,12 +1,13 @@
-from flask import Flask  # Import Flask to allow us to create our app
-# Create a new instance of the Flask class called "app"
-app = Flask(__name__)
+from flask import Flask, render_template  # added render_template!
+app = Flask(__name__)  
 
 
 # The "@" decorator associates this route with the function immediately following
-@app.route('/')
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/')                           
+def index():
+    # Instead of returning a string, 
+    # we'll return the result of the render_template method, passing in the name of our HTML file
+    return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
 
 # import statements, maybe some other routes
 @app.route('/success')
