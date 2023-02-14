1dcfa994e3bb6eebf4cb282b59e878d4833b0851
Raspber <hpachec1@gmail.com>
2023-02-13 21:38:27 -0800

    commit

--- flask/great_number_game/server.py
+++ flask/great_number_game/server.py
@@ -1,13 +1,24 @@
-from flask import Flask, redirect, render_template, session, random
+from flask import Flask, redirect, render_template, session,request
 app = Flask(__name__)
 app.secret_key = '123123'  # set a secret key for security purposes
-random.randint(1, 100)
+import random
 
 @app.route('/')
 def index():
-    session = random.randint(1,100)
+    if 'random_num' not in session:
+        session['random_num'] = random.randint(1,100)
+    print(session)
     return render_template('index.html')
 
+@app.route('/guess', methods =['POST'])
+def guess():
+    session['guess'] = int(request.form['guess'])
+    return redirect('/')
+
+@app.route('/play_again')
+def play_again():
+    session.clear()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
